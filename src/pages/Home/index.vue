d6287f061f3358eabf9ff76f12cbc4109a751c10
Henrique <hld@henriquedutra.com.br>
2018-06-20 15:23:04 -0300

    Add Current points to home page
    Signed-off-by: gabrielsclimaco <gabrielsclimaco@gmail.com>
    Signed-off-by: Henrique <hld@henriquedutra.com.br>

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
-q-page#home.column.justify-center.items-center
-  qriously(:value='cpf' :size='250')
+q-page#home
+  current-points(:userName='user.name' :points='points')
+  div.column.justify-center.items-center
+    span.message.text-center.q-mx-md Apresente esse código de barras na máquina para inserir
+      | garrafas e coletar pontos
+    q-card(color='white').q-mt-md
+      q-card-main
+        qriously(:value='String(user.cpf)' :size='230')
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
+
+import CurrentPoints from '../../components/CurrentPoints';
 
 export default {
   name: 'home',
   components: {
+    CurrentPoints,
   },
   async beforeRouteEnter(to, from, next) {
     try {
@@ -20,20 +29,32 @@ export default {
   props: {
   },
   data() {
-    return {};
+    return {
+      cpf: '',
+    };
   },
   computed: {
     ...mapGetters([
       'user',
+      'points',
     ]),
-    cpf() {
-      return this.user.cpf ? this.user.cpf : '';
-    },
   },
   methods: {
+    ...mapActions([
+      'fetchOperations',
+    ]),
+  },
+  mounted() {
+    this.cpf = this.user.cpf;
+    this.fetchOperations();
   },
 };
 </script>
 
 <style lang='stylus' scoped>
+#home
+  display grid
+  grid-template-rows 1.5fr 8.5fr
+  .message
+    color #7D8597
 </style>
